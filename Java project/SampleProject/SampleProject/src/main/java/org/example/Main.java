26fe7fb2b2bdd1cca737f3c24cb5e2d1ec107bd9
Teen <118597396+wymanteen@users.noreply.github.com>
2022-11-19 17:58:20 +0000

    added Person class

--- Java project/SampleProject/SampleProject/src/main/java/org/example/Main.java
+++ Java project/SampleProject/SampleProject/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.model.Person;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        //Create a Person obj
+        Person p1 = new Person(0, "John", "UK");
+        p1.printMyName();
     }
 }
