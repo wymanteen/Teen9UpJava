83e16b1e9e2da642c22bce15b189fcdf7be37cef
Teen <118597396+wymanteen@users.noreply.github.com>
2022-11-19 18:27:32 +0000

    updated Java project

---
+++ Java project/SampleProject/SampleProject/src/main/java/org/example/interfaces/Actions.java
@@ -0,0 +1,8 @@
+package org.example.interfaces;
+
+//interface is a kind of abstract classes
+
+public interface Actions {
+    public void goToSomewhere(String location); // interface method (does not have a body)
+    public void sleep(); // interface method (does not have a body)
+}
