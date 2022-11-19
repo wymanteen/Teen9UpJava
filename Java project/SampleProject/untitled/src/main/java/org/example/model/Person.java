153382029c1296a93229e3487d48a2585ce0e708
Teen <118597396+wymanteen@users.noreply.github.com>
2022-11-19 17:29:38 +0000

    create a sample project

---
+++ Java project/SampleProject/untitled/src/main/java/org/example/model/Person.java
@@ -0,0 +1,38 @@
+package org.example.model;
+
+public class Person {
+
+    //attributes
+    public int id;
+    private String name;
+    private String address;
+
+    //constructor has no return type!!!
+    public Person(int id, String name, String address){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    //methods (A function in a class called method)
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public void printMyName(){
+        System.out.println("My name is " + this.name);
+    }
+
+}
